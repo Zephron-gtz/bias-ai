c1702fb0da41f900c7e2efd403250b57ac14880e
Zephron-gtz <efrain.j.gutierrez@outlook.com>
2018-04-16 13:27:41 -0600

    update

--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from mysite.models import Post
 from django.http import HttpResponse
 # Create your views here.
+
+from django.utils import timezone
+
 
 def index(request):
     return render(request, 'index.html')
 def more(request):
-    return render(request, 'more.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'more.html', {'posts': posts})
+
